9cfbd334371f3c46cb53bf822995557af63f8e49
HadassaAvimor <h0533185978@gmail.com>
2024-01-29 23:07:24 +0200

    refactor project stracture

---
+++ client/src/components/RMGButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface RMGButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const RMGButton: React.FC<RMGButtonProps> = ({ onClick, children }) => {
+  return (
+    <button 
+      onClick={onClick}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default RMGButton;
